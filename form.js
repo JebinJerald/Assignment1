fc619b75d2737678c4cebd318a0122f8e263b829
JebinJerald <jebin7786@gmail.com>
2022-01-18 16:20:03 +0530

    valid

--- form.js
+++ form.js
@@ -1,28 +1,51 @@
-let email = document.getElementById("email");
-let pwd = document.getElementById("pwd");
-let emailerror = document.getElementById("emailerror");
-let pwderror = document.getElementById("pwderror");
-let email1 = document.getElementById("email1");
+let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
 
+// signup
 function check(){
-    let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
+    let email = document.getElementById("email");
+    let pwd = document.getElementById("pwd");
+    let emailerror = document.getElementById("emailerror");
+    let conpwd = document.getElementById("conpwd");
+    let conpwderror = document.getElementById("conpwderror");
+    let phone = document.getElementById("phone");
+    let phonerror = document.getElementById("phonerror");
+    let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
     if(regexp.test(email.value)){
-        return true;
-    }
-    else{
+        if(phoneno.test(phone.value)){
+            return true;
+        } else{
+            phonerror.innerHTML = "Phone no. doesn't match criteria";
+            phonerror.style.color = "red";
+        } 
+        if(pwd.value==conpwd.value){
+            return true;
+        } else{
+            conpwderror.innerHTML = "Passwords doesn't match";
+            conpwderror.style.color = "red";
+            return false;
+        }
+    } else{
         emailerror.innerHTML = "Email entered is invalid";
         emailerror.style.color = "red";
         return false;
     }
 }
+// /signup
+
+// login
 function check1(){
-    let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
+    let email1 = document.getElementById("email1");
+    let emailerror1 = document.getElementById("emailerror1");
+    let pwd1 = document.getElementById("pwd1");
+    let pwderror1 = document.getElementById("pwderror1");
+
     if(regexp.test(email1.value)){
         return true;
-    }
-    else{
-        emailerror.innerHTML = "Email entered is invalid";
-        emailerror.style.color = "red";
+    } else{
+        emailerror1.innerHTML = "Email entered is invalid";
+        emailerror1.style.color = "red";
         return false;
     }
 }
+// /login
