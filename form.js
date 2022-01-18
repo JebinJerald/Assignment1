69c8089b3187307c4b0ec0dbe18d3fb9ea689f03
JebinJerald <jebin7786@gmail.com>
2022-01-18 17:18:38 +0530

    form2

--- form.js
+++ form.js
@@ -1,10 +1,12 @@
-let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
+let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/;
+let pwdregexp = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
 
 // signup
 function check(){
     let email = document.getElementById("email");
     let pwd = document.getElementById("pwd");
     let emailerror = document.getElementById("emailerror");
+    let pwderror = document.getElementById("pwderror");
     let conpwd = document.getElementById("conpwd");
     let conpwderror = document.getElementById("conpwderror");
     let phone = document.getElementById("phone");
@@ -12,12 +14,19 @@ function check(){
     let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
     if(regexp.test(email.value)){
+        if(pwdregexp.test(pwd.value)){
+            return true;
+        } else{
+            pwderror.innerHTML = "Password entered doesn't meet criteria";
+            pwderror.style.color = "red";
+            return false;
+        }
         if(phoneno.test(phone.value)){
             return true;
         } else{
-            phonerror.innerHTML = "Phone no. doesn't match criteria";
+            phonerror.innerHTML = "Phone no. doesn't meet criteria";
             phonerror.style.color = "red";
-        } 
+        }
         if(pwd.value==conpwd.value){
             return true;
         } else{
@@ -41,6 +50,13 @@ function check1(){
     let pwderror1 = document.getElementById("pwderror1");
 
     if(regexp.test(email1.value)){
+        if(pwdregexp.test(pwd1.value)){
+            return true;
+        } else{
+            pwderror1.innerHTML = "Password entered doesn't meet criteria";
+            pwderror1.style.color = "red";
+            return false;
+        }
         return true;
     } else{
         emailerror1.innerHTML = "Email entered is invalid";
